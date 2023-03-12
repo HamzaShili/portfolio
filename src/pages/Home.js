6b016b958282b74848a301ea4769402446de0bae
hamza.shili <hamzashili5@gmail.com>
2023-03-12 16:21:28 +0100

    change name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='alert alert-info' role="alert">
       <img src="/images/photo-1.jpg" width="150px" alt='Avatar' />
       <h1>Hamza Shili</h1>
-      <p>Hello my name is Jane Doe, I'am a Fullstack Web Developper</p>
+      <p>Hello my name is Hamza Shili, I'am a Fullstack Web Developper</p>
       <img src={webImage} alt="Web" style={{ width: "100%" }} />
     </div>
   )
